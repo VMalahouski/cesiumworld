ecf3fbaf04ad7bed5ff2285f54541ae378c0aad7
alex.oreshkevich <alex.oreshkevich@redsoft.pro>
2015-01-08 12:57:13 +0300

    add gold colors & checking jsons

--- worldApp/js/init.js
+++ worldApp/js/init.js
@@ -1,20 +1,43 @@
-var viewer;
-initCesiumPlanet();
+var viewer,
+  Init = function () {
+  };
+
+Init.prototype.init = function (dataUrls) {
+  if (dataUrls === undefined) {
+    dataUrls = {
+      chn: {
+        jsonUrl: "../../Apps/SampleData/chn.json",
+        catalogUrl: "https://www.google.ru/"
+      },
+      rus: {
+        jsonUrl: "../../Apps/SampleData/rus.json",
+        catalogUrl: "http://www.tut.by/"
+      },
+      blr: {
+        jsonUrl: "../../Apps/SampleData/blr.json",
+        catalogUrl: "http://www.onliner.by/"
+      }
+    }
+  }
+
+  initCesiumPlanet();
+
+  // Инициализация по каждому json
+  for (var data in dataUrls) {
+    initColors(dataUrls[data]);
+//    addCatalogAction(dataUrls[data]); not use now
+  }
+};
+
+var d = new Init();
+d.init(undefined);
 
 /**
  * initialization planet and parts
  */
 function initCesiumPlanet() {
-  var chinaUrl = '../../Apps/SampleData/chn.json',
-    russiaUrl = '../../Apps/SampleData/rus.json',
-    belarusUrl = '../../Apps/SampleData/blr.json';
+
   viewer = new Cesium.Viewer('cesiumContainer');
-//  viewer = new Cesium.Viewer('cesiumContainer', {
-//    imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
-//      url: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
-//    }),
-//    baseLayerPicker: false
-//  });
 
   // Включаем возможность читать json выбранной области
   Cesium.viewerEntityMixin(viewer);
@@ -22,19 +45,10 @@ function initCesiumPlanet() {
   //Устанавливаем точку обзора
   viewer.scene.camera.lookAt(Cesium.Cartesian3.fromDegrees(75, 45, 14000000),
     Cesium.Cartesian3.fromDegrees(75, 50, 0), Cesium.Cartesian3.UNIT_Z);
-  // Добавляем json карты
-//viewer.dataSources.add(Cesium.GeoJsonDataSource.fromUrl(russiaUrl));
-//viewer.dataSources.add(Cesium.GeoJsonDataSource.fromUrl(chinaUrl));
-//viewer.dataSources.add(Cesium.GeoJsonDataSource.fromUrl(belarusUrl));
   // Включаем границу дня/ночи
   viewer.scene.globe.enableLighting = true;
-  // Раскрашиваем страны
-  initColors(russiaUrl);
-  initColors(chinaUrl);
-  initColors(belarusUrl);
-
-  eventBox();
-
+  // убираем надпись "Cesium"
+  $('.cesium-widget-credits').css({'display': 'none'})
 }
 
 /**
@@ -45,60 +59,66 @@ function initColors(jsonUrl) {
 
   var dataSource = new Cesium.GeoJsonDataSource();
   viewer.dataSources.add(dataSource);
+  viewer.scene.primitives.destroyPrimitives = false;
 
-  dataSource.loadUrl(jsonUrl).then(function () {
+  // глубокий синий: #25059B
+  // red 37 green 5 blue 155
+  // золотой: #FFD700
+  // red 255 green 215 blue 0
+
+  dataSource.loadUrl(jsonUrl.jsonUrl).then(function () {
     // Получаем массив объектов
     var entities = dataSource.entities.entities,
-      colorHash = {};
+      color = Cesium.Color.GOLD;
+    color.alpha = 0.5;
 
-    var color = Cesium.Color.fromRandom({
-      alpha: 0.4
-    });
-    colorHash[entities[0].name] = color;
+//    if (!color) {
+//      color = Cesium.Color.fromRandom({
+//        alpha : 0.4
+//      });
+//      colorHash[entities[0].name] = color;
+//    }
 
     for (var i = 0; i < entities.length; i++) {
       entities[i].polygon.material = Cesium.ColorMaterialProperty.fromColor(color);
 //      Поднять область в зависимости от численности населения штата (). Каждый объект хранит свойства для функции GeoJSON он был создан Поскольку население огромное количество, мы делим на 50.
 //      entity.polygon.extrudedHeight = new Cesium.ConstantProperty(entity.properties.Population / 50.0);
     }
-//    for (var i = 0; i < entities.length; i++) {
-//      // Для каждого объекта, создать случайный цвет, основанный на имени государства.
-//      // Некоторые государства имеют несколько объектов,
-//      // поэтому мы накапливаем цвет в хэш, так что мы используем тот же цвет для всего государства.
-//      var entity = entities[i],
-//        name = entity.name,
-//        color = colorHash[name];
-//      if (!color) {
-//        color = Cesium.Color.fromRandom({
-//          alpha : 0.3
-//        });
-//        colorHash[name] = color;
-//      }
-//
-//      //Устанавливаем polygon.material для нашего случайного цвета.
-//      entity.polygon.material = Cesium.ColorMaterialProperty.fromColor(color);
-//
-
-//    }
   });
 }
 
-function MyObject() {};
-MyObject.prototype.myListener = function (arg1, arg2) {
-  this.myArg1Copy = arg1;
-  this.myArg2Copy = arg2;
-  console.log('My listener\t' + arg1 + '\t' + arg2);
-};
+function addCatalogAction(dataUrls) {
 
-function eventBox() {
-  console.log('eventBox');
-  var myArg1Copy, myArg2Copy;
+//    addPolygon()
+  var handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+//  handler.destroy();
+  handler.setInputAction(function () {
+    alert("go to " + dataUrls.catalogUrl);
+  }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
 
+  var primitives = viewer.scene.primitives;
 
-  var myObjectInstance = new MyObject();
-  var evt = new Cesium.Event();
-  evt.addEventListener(MyObject.prototype.myListener, myObjectInstance);
-  evt.raiseEvent('1', '2');
-  evt.removeEventListener(MyObject.prototype.myListener);
-  console.log('eventBox end');
+  handler.setInputAction(function(movement) {
+    // clear picked flags
+    var numberOfPrimitves = primitives.length;
+    for (var i = 0; i < numberOfPrimitves; ++i) {
+      var p = primitives.get(i);
+      p.processedPick = false;
+    }
+
+    // get an array of all primitives at the mouse position
+    var pickedObjects = viewer.scene.drillPick(movement.endPosition);
+    if(Cesium.defined(pickedObjects)) {
+      console.log('Cesium.defined(pickedObjects) = true');
+      for( i=0; i<pickedObjects.length; ++i) {
+        var polygon = pickedObjects[i].primitive;
+        if(polygon.picked === false) {
+          console.log('polygon.picked = false!!!!!!!!!!!!!');
+        }else {
+          console.log('polygon.picked = true!!!!!!!!!!!!!');
+        }
+      }
+    }
+
+  }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
 }
